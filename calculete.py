2006c4a31cb99e44bd416405af207a6e3d94a0c4
lenolium3006 <baikova-elena2006@mail.ru>
2025-10-14 12:08:13 +0300

    Update calculete.py

--- calculete.py
+++ calculete.py
@@ -2,28 +2,24 @@ import unittest
 
 def calculate(a, b, epsilon=0.0001):
     if b == 0:
-        print("Ошибка: деление на ноль")
-        return None
+        return None  
     return round(a / b, 4)
 
 
 def load_params(filename="settings.ini"):
-    try:
-        f = open(filename, "r")
-        for line in f:
-            if "epsilon" in line:
-                parts = line.split("=")
-                f.close()
-                return float(parts[1])
-        f.close()
+    f = open(filename, "r")
+    for line in f:
+        if "epsilon" in line:
+            return float(line.split("=")[1])
+    f.close()
 
-
+# тесты
 class TestAll(unittest.TestCase):
     def test_division_half(self):
-        self.assertEqual(calculate(1, 2, 0.1), 0.5)
+        self.assertEqual(calculate(1, 2), 0.5)
 
     def test_small_division(self):
-        self.assertEqual(calculate(1, 1000, 0.001), 0.001)
+        self.assertEqual(calculate(1, 1000), 0.001)
 
     def test_divide_by_zero(self):
         self.assertIsNone(calculate(1, 0))
@@ -34,11 +30,5 @@ class TestAll(unittest.TestCase):
         e = load_params("settings.ini")
         self.assertEqual(e, 0.001)
 
-    def test_file_not_found(self):
-        e = load_params("nofile.ini")
-        self.assertIsNone(e)
-
-
 if __name__ == "__main__":
     unittest.main()
-
